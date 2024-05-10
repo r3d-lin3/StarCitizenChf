1f909638b793ae6fcd370f7f96c3c107a2b86397
r3d-lin3 <tcrtyler@gmail.com>
2024-05-10 00:21:30 -0500

    Update extract_actors.ps1

--- extract_actors.ps1
+++ extract_actors.ps1
@@ -1,4 +1,4 @@
-$datap4k = "C:\Program Files\Roberts Space Industries\StarCitizen\EPTU\Data.p4k"
+$datap4k = "D:\Roberts Space Industries\StarCitizen\EPTU\Data.p4k"
 Write-Host "Extracting dcbs from Data.p4k"
 unp4k $datap4k *.dcb
 Write-Host "Extracting datafrom dcbs"
